4ad0bc2a2bdefa7f1260cab9ffb3821eadd45c95
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2012-03-03 17:52:00 +0000

    added
    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/Apertus/AndroidRemote@325 46015a06-04f4-4860-9c33-90e0f15d9639

---
+++ src/Appertus/Test/APPertusTestActivity.java
@@ -0,0 +1,144 @@
+package Appertus.Test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class APPertusTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    private Button mReadButton;
+    private TextView mTextresult;
+    private TextView mIPField;
+    private TextView mSliderPos;
+    private ApertusSlider mShutterTime;
+
+    private int CameraImageWidth;
+    private int CameraImageHeight;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+
+	// Evil Workaround for now
+	ThreadPolicy tp = ThreadPolicy.LAX;
+	StrictMode.setThreadPolicy(tp);
+
+	mReadButton = (Button) findViewById(R.id.button1);
+	mReadButton.setOnClickListener(new View.OnClickListener() {
+	    public void onClick(View v) {
+		try {
+		    UpdateCameraData();
+		} catch (Exception e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		}
+	    }
+	});
+	mTextresult = (TextView) findViewById(R.id.result1);
+	mIPField = (TextView) findViewById(R.id.IPfield);
+	mShutterTime = (ApertusSlider) findViewById(R.id.slider1);
+	mShutterTime.getScrollX();
+	mSliderPos = (TextView) findViewById(R.id.slidepos);
+	
+	mShutterTime.SetTextView(mSliderPos);
+    }
+    
+    public void UpdateSlidePosText(){
+    
+    }
+
+    public void UpdateCameraData() throws Exception {
+	URLConnection conn = null;
+	BufferedReader data = null;
+	String line;
+	String result;
+	StringBuffer buf = new StringBuffer();
+	URL CameraUrl = null;
+	String camera_url = "http://" + mIPField.getText() + "/ElphelVision/elphelvision_interface.php";
+	try {
+	    CameraUrl = new URL(camera_url);
+	} catch (MalformedURLException e) {
+	    System.out.println("Bad URL: " + CameraUrl);
+	}
+
+	int i = 0;
+	try {
+	    conn = CameraUrl.openConnection();
+	    conn.connect();
+
+	    data = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+	    buf.delete(0, buf.length());
+	    while ((line = data.readLine()) != null) {
+		buf.append(line + "\n");
+	    }
+
+	    result = buf.toString();
+	    data.close();
+
+	    // try to extract data from XML structure
+
+	    try {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+
+		Document doc = db.parse(new ByteArrayInputStream(result.getBytes()));
+		doc.getDocumentElement().normalize();
+		NodeList nodeLst = doc.getElementsByTagName("elphel_vision_data");
+		for (int s = 0; s < nodeLst.getLength(); s++) {
+		    Node fstNode = nodeLst.item(s);
+		    if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+			Element fstElmnt = (Element) fstNode;
+			NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("image_width");
+			Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
+			NodeList fstNm = fstNmElmnt.getChildNodes();
+			CameraImageWidth = Integer.parseInt(((Node) fstNm.item(0)).getNodeValue());
+
+			NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("image_height");
+			Element lstNmElmnt = (Element) lstNmElmntLst.item(0);
+			NodeList lstNm = lstNmElmnt.getChildNodes();
+			CameraImageHeight = Integer.parseInt(((Node) lstNm.item(0)).getNodeValue());
+		    }
+		}
+		// Create final Text
+		String Content = "Image Width: " + CameraImageWidth + "\n";
+		Content += "Image Height: " + CameraImageHeight + "\n";
+		mTextresult.setText(Content);
+	    } catch (Exception e) {
+		e.printStackTrace();
+		System.out.println("error1");
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.out.println("error2");
+	}
+    }
+}
---
+++ src/Appertus/Test/ApertusSlider.java
@@ -0,0 +1,107 @@
+package Appertus.Test;
+
+import java.util.ArrayList;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ApertusSlider extends HorizontalScrollView {
+    private static final int SWIPE_MIN_DISTANCE = 5;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 300;
+    private TextView ShowPos;
+
+    private GestureDetector mGestureDetector;
+    private int mActiveFeature = 0;
+
+    public ApertusSlider(Context context, AttributeSet attrs, int defStyle) {
+	super(context, attrs, defStyle);
+	this.setVerticalScrollBarEnabled(false);
+	this.setHorizontalScrollBarEnabled(false);
+	Init();
+    }
+
+    public ApertusSlider(Context context, AttributeSet attrs) {
+	super(context, attrs);
+	this.setVerticalScrollBarEnabled(false);
+	this.setHorizontalScrollBarEnabled(false);
+	Init();
+    }
+
+    public ApertusSlider(Context context) {
+	super(context);
+	this.setVerticalScrollBarEnabled(false);
+	this.setHorizontalScrollBarEnabled(false);
+	Init();
+    }
+
+    public void SetTextView(TextView label) {
+	ShowPos = label;
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+	// TODO Auto-generated method stub
+	// Log.i("Scrolling", "X from ["+oldl+"] to ["+l+"]");
+	ShowPos.setText("Position: " + l);
+	super.onScrollChanged(l, t, oldl, oldt);
+    }
+
+    public void Init() {
+	setOnTouchListener(new View.OnTouchListener() {
+	    @Override
+	    public boolean onTouch(View v, MotionEvent event) {
+		// If the user swipes
+		if (mGestureDetector.onTouchEvent(event)) {
+		    System.out.println("onTouchEvent");
+		    return true;
+		} else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
+		    // Snap to closes value
+		    int scrollX = getScrollX();
+		    int featureWidth = 100;// Snapping to
+		    mActiveFeature = ((scrollX + (featureWidth / 2)) / featureWidth);
+		    int scrollTo = mActiveFeature * featureWidth;
+		    smoothScrollTo(scrollTo, 0);
+		    System.out.println("Snapping to: " + scrollTo);
+		    return true;
+		} else {
+		    return false;
+		}
+	    }
+	});
+	mGestureDetector = new GestureDetector(new MyGestureDetector());
+    }
+
+    class MyGestureDetector extends SimpleOnGestureListener {
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+	    try {
+		// right to left
+		if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+		    int featureWidth = getMeasuredWidth();
+		    //mActiveFeature = (mActiveFeature < (mItems.size() - 1)) ? mActiveFeature + 1 : mItems.size() - 1;
+		    //smoothScrollTo(mActiveFeature * featureWidth, 0);
+		    return true;
+		}
+		// left to right
+		else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+		    int featureWidth = getMeasuredWidth();
+		    mActiveFeature = (mActiveFeature > 0) ? mActiveFeature - 1 : 0;
+		    smoothScrollTo(mActiveFeature * featureWidth, 0);
+		    return true;
+		}
+	    } catch (Exception e) {
+
+		Log.e("Fling", "There was an error processing the Fling event:" + e.getMessage());
+	    }
+	    return false;
+	}
+    }
+}
